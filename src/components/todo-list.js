c3d291ca55e63d1725b780bf1dfcacbb9bbd7a28
gabdir <a.gabdrahmanov@innopolis.ru>
2020-03-22 18:59:13 +0300

    Adding css

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import TodoListItem from './todo-list-item'
+import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos }) => {
 
     const elements = todos.map((item) => {
-        const {id, ... items} = item
-        return (<li key={id}><TodoListItem {... items}/></li>);
+        const { id, ...itemProps } = item;
+
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem {...itemProps } />
+            </li>
+        );
     });
+
     return (
-        <ul>
-            {elements}
+        <ul className="list-group todo-list">
+            { elements }
         </ul>
     );
 };
